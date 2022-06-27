ccd1bfcfe39904612a72aac9392660f1bd97e9c3
AleksandrKolenov <kolenov.aa@yandex.ru>
2022-06-27 22:47:50 +0300

    dt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import Counter from "./Counter/Counter";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  <>
-  <h1>Состояние компонента</h1>
-
-    <Counter />
-    
-    </>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
